be468619e02b0c14ad78736a905f4d638a955698
Jardel Costa <jardel.costa056@gmail.com>
2020-09-25 10:12:56 -0300

    ajustes layout mobile finalizados

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import logo from '../../assets/images/logo.png'
-
+import {Link} from 'react-router-dom'
 import './index.css'
 const Home = ({children}) => {
     return (
         <header id="container-header">
             <div className="content-header">
                 <div className="logo">
-                    <a href="/">
+                    <Link to="/">
                         <img src={logo} />
-                    </a>            
+                    </Link>            
                 </div>
                 {children}
             </div>
