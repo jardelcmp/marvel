917ad30f2d3b184865003b5c6a3789e7461094f5
Jardel Costa <jardel.costa056@gmail.com>
2020-09-26 20:47:53 -0300

    #

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ import './index.css'
 function Footer() {
     return (
         <section id="footer">
-        <a href="https://github.com/jardelcmp" target="_blank"><AiFillGithub size={24}/></a>
+        <a href="https://github.com/jardelcmp/marvel" target="_blank"><AiFillGithub size={24}/></a>
         <a href="https://www.linkedin.com/in/jardel-costa-m-paula/" target="_blank"><AiFillLinkedin size={24}/></a>
     </section>
     )
